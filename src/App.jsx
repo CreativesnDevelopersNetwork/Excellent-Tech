6d54651448d8913a64caa8e350c37ff7820b2c99
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-08 01:20:34 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
-import CustomerSignup from "./routes/CustomerSignup";
-import BusinessSignup from "./routes/BusinessSignup";
-import Accomodation from "./routes/Accomodation";
-import Hotels from "./routes/Hotels";
-import CarRental from "./routes/CarRental";
-import Flights from "./routes/Flights";
-import React from "react";
+
 
 function App() {
   return (
@@ -16,12 +10,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/customersignup" element={<CustomerSignup />} />
-        <Route path="/businesssignup" element={<BusinessSignup />} />
-        <Route path="/accomodation" element={<Accomodation />} />
+      
+        {/* <Route path="/accomodation" element={<Accomodation />} />
         <Route path="/hotels" element={<Hotels />} />
         <Route path="/carrental" element={<CarRental />} />
-        <Route path="/flights" element={<Flights />} />
+        <Route path="/flights" element={<Flights />} /> */}
       </Routes>
     </div>
   );
