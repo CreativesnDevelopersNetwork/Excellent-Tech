28452bb82499ebb2e8f4a42c2c73512bc6dc57ff
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-08-29 13:55:17 +0000

    app commit

--- src/components/AdminDashboard/Sidebar.js
+++ src/components/AdminDashboard/Sidebar.js
@@ -6,11 +6,14 @@ function Sidebar() {
     return(
         <>
          <div className="app-sidebar">
-      <Link to="/" className="app-sidebar-link active">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home">
+      <Link to="/" className="barLink active">
+         <div className="app-sidebar-link">
+         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home">
           <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
           <polyline points="9 22 9 12 15 12 15 22" />
        </svg>
+         </div>
+         <span>Home</span>
       </Link>
       <Link href="/" className="app-sidebar-link">
         <svg className="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"  viewBox="0 0 24 24">
